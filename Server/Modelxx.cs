c12c384f4f4b96b1f932cea8bcb098b0f026b166
nonanik112 <metehan_1306@hotmail.com>
2022-08-25 16:26:37 +0300

    Devam Network

---
+++ Server/Modelxx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+
+    class Modelxx
+    {
+        //[DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        //public static extern IntPtr Byte64(IntPtr lpAddress,
+        //        uint dwSize,
+        //        uint flAllocationType,
+        //        uint flProtect);
+    }
+}
